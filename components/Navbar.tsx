dd047be4354b1867100d31a4b1bee1cf545a6131
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-28 22:20:02 -0500

    Added name and nav items to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -28,18 +28,22 @@ const NavItems: Array<NavItem> = [
 const Navbar = () => {
     const {systemTheme, theme, setTheme } = useTheme();
     const currentTheme = theme === "system" ? systemTheme : theme;
-    const [isMobileView, setMobileView] = useState(false);
+    const [isMobileNav, setMobileNav] = useState(false);
 
     return (
-        <header>
-            <div>
+        <header className="w-full mx-auto px-20 md:px-4 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+            <div className='md:flex md:items-center justify-between'>
                 <div>
-                    <div>
-                        <h2>David Zhao</h2>
+                    <div className='md:py-5 md:block'>
+                        <h2 className='text-2xl font-bold'>David Zhao</h2>
                     </div>
                 </div>
-                <div>
-
+                <div className='md:flex md:space-x-6'>
+                    {
+                        NavItems.map((item, idx) => {
+                            return <a key={idx}>{item.label}</a>
+                        })
+                    }
                 </div>
             </div>
         </header>
