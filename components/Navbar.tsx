d8ec7390198a75c4826713f2a8e502dd989ec664
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-29 18:09:26 -0500

    Create mobile nav menu

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -30,16 +30,23 @@ const Navbar = () => {
     const [isMobileNav, setMobileNav] = useState(false);
 
     return (
-        <header className={`w-full mx-auto px-20 md:px-4 fixed top-0 z-50 shadow ${
+        <header className={`w-full mx-auto px-4 fixed top-0 z-50 shadow ${
             theme === 'dark' ? 'bg-stone-900 border-b border-stone-600' : 'bg-white '
           }`}>
             <div className='md:flex md:items-center justify-between'>
-                <div>
+                <div className='flex items-center justify-between py-3'> 
                     <div className='md:py-5 md:block cursor-pointer'>
                         <h2 className='text-2xl font-bold'>David Zhao</h2>
                     </div>
+                    <button
+                        className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border md:hidden"
+                        onClick={() => setMobileNav(!isMobileNav)}
+                    >
+                        {isMobileNav ? <IoClose size={20} /> : <IoMenu size={20} />}
+                    </button>
                 </div>
-                <div className='md:flex items-center justify-center md:space-x-6 cursor-pointer'>
+
+                <div className={`md:flex items-center justify-center md:space-x-6 cursor-pointer ${isMobileNav ? "block" : "hidden"}`}>
                     {
                         NavItems.map((item, idx) => {
                             return <a key={idx}>{item.label}</a>
