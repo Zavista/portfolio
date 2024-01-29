dbe68cd21ebf7495dd78f727d68550367302c3c0
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-28 21:57:09 -0500

    Import items to use

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Link from 'next/link'
+import { useTheme } from 'next-themes'
+import { RiMoonFill, RiSunLine } from 'react-icons/ri'
+import { IoMenu, IoClose } from 'react-icons/io5'
+
+
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+    </div>
   )
 }
 
