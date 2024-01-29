65f284008cd28c586004944ef02c08348ccbbfc3
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-29 18:37:10 -0500

    Import HomeSection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
 import Navbar from "../components/Navbar";
+import HomeSection from "../components/HomeSection";
 import { ThemeProvider } from "next-themes";
 
 export default function Home() {
   return (
     <ThemeProvider defaultTheme="light">
       <Navbar></Navbar>
+      <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+        <HomeSection></HomeSection>
+      </main>
+
     </ThemeProvider>
       
   );
