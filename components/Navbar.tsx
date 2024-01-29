74210e5b394c2fa9376459b5c92846690d18cee3
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-28 22:02:28 -0500

    Created NavItems

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,26 @@ import { useTheme } from 'next-themes'
 import { RiMoonFill, RiSunLine } from 'react-icons/ri'
 import { IoMenu, IoClose } from 'react-icons/io5'
 
-
+interface NavItem {
+    label: string;
+    page: string;
+  }
+  
+const NavItems: Array<NavItem> = [
+    {
+      label: "Home",
+      page: "home",
+    },
+    {
+      label: "About",
+      page: "about",
+    },
+    {
+      label: "Projects",
+      page: "projects",
+    },
+];
+  
 
 const Navbar = () => {
   return (
