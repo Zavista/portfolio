f36a5816cced116a3f8544bf8378d9426931c345
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-29 19:08:58 -0500

    Import About Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import HomeSection from "../components/HomeSection";
 import { ThemeProvider } from "next-themes";
+import AboutSection from "@/components/AboutSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar></Navbar>
       <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
         <HomeSection></HomeSection>
+        <AboutSection></AboutSection>
       </main>
 
     </ThemeProvider>
