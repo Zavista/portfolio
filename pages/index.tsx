cd4df8aab65a9de108d5630da8c635392ef028bd
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-29 19:02:55 -0500

    Add styling to main

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <ThemeProvider defaultTheme="light">
       <Navbar></Navbar>
-      <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+      <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
         <HomeSection></HomeSection>
       </main>
 
