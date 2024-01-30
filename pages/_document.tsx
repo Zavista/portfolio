56d56942fa51af9bf9d671fe5ffa51b8a928688f
ZhaDav <84294353+Zavista@users.noreply.github.com>
2024-01-29 20:50:53 -0500

    Updated Head title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>David Zhao | Portfolio </title>
       </Head>
       <body>
         <Main />
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,15 @@ import { ThemeProvider } from "next-themes";
 import AboutSection from "@/components/AboutSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import Footer from '../components/Footer'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <ThemeProvider defaultTheme="light">
+    <>
+      <Head>
+        <title>David Zhao | Portfolio </title>
+      </Head>
+      <ThemeProvider defaultTheme="light">
       <Navbar></Navbar>
       <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
         <HomeSection></HomeSection>
@@ -17,5 +22,7 @@ export default function Home() {
       <Footer></Footer>
     </ThemeProvider>
       
+    </>
+
   );
 }
